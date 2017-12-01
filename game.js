a39325b1ddcd01f8827e933920a5013615b894fd
DanielMathis <daniel.mathis@sector8.eu>
2017-12-02 00:25:04 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,128 @@
+var ASSET_VERSION = (new Date()).getTime();
+var BASE_PATH = 'assets/';
+
+var player;
+var cursors;
+
+var state = {
+    preload: function () {
+        game.load.image('background', BASE_PATH + 'debug-grid-1920x1920.png?' + ASSET_VERSION);
+        game.load.image('player', BASE_PATH + 'santa.png?' + ASSET_VERSION);
+    },
+    create: function () {
+
+        game.add.tileSprite(0, 0, 1920, 1920, 'background');
+
+        game.world.setBounds(0, 0, 1920, 1920);
+
+        game.physics.startSystem(Phaser.Physics.P2JS);
+
+        player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+        player.scale.setTo(0.3, 0.3); //verkleinert das Playerimage
+        player.anchor.set(0.5);
+
+        // game.physics.arcade.enable(player);
+
+        game.physics.p2.enable(player);
+
+        player.body.fixedRotation = true;
+
+        cursors = game.input.keyboard.createCursorKeys();
+
+        //  Notice that the sprite doesn't have any momentum at all,
+        //  it's all just set by the camera follow type.
+        //  0.1 is the amount of linear interpolation to use.
+        //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
+        game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+    },
+    update: function () {
+        player.body.setZeroVelocity();
+
+        if (cursors.up.isDown) {
+            player.body.moveUp(300)
+        }
+        else if (cursors.down.isDown) {
+            player.body.moveDown(300);
+        }
+
+        if (cursors.left.isDown) {
+            player.body.velocity.x = -300;
+        }
+        else if (cursors.right.isDown) {
+            player.body.moveRight(300);
+        }
+
+
+        // if (cursors.up.isDown)
+        // {
+        //     game.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
+        // }
+        // else
+        // {
+        //     player.body.acceleration.set(0);
+        // }
+        //
+        // if (cursors.left.isDown)
+        // {
+        //     player.body.angularVelocity = -300;
+        // }
+        // else if (cursors.right.isDown)
+        // {
+        //     player.body.angularVelocity = 300;
+        // }
+        // else
+        // {
+        //     player.body.angularVelocity = 0;
+        // }
+
+
+    }, render: function () {
+        game.debug.cameraInfo(game.camera, 32, 32);
+    },
+    start: function () {
+
+    },
+    reset: function () {
+
+    },
+    addScore: function (addWhat) {
+        this.score += addWhat;
+        this.scoreText.setText("SCORE: " + this.score);
+    },
+    showHint: function (focusOn, text) {
+        var hint = this.game.add.text(
+            focusOn.x,
+            focusOn.y,
+            text,
+            {
+                fill: '#ffdd00',
+                align: 'center'
+            }
+        );
+        hint.anchor.setTo(0.5, 0.5);
+        hint.fontSize = 20;
+
+        var move = this.game.add.tween(hint);
+        move.to({y: hint.y - 100, x: hint.x - 100 * Math.random() + 50}, 1000);
+        move.onComplete.add(function () {
+            hint.kill()
+        }, this);
+        move.start();
+
+    }
+    // setGameOver: function () {
+    //     this.timeOver = this.game.time.now;
+    //     this.gameOver = true;
+    //
+    //     this.scoreText.setText("FINAL SCORE: " + this.score + "\nTOUCH TO TRY AGAIN");
+    // }
+};
+
+
+var game = new Phaser.Game(
+    800,
+    600,
+    Phaser.CANVAS,
+    document.querySelector('#screen'),
+    state
+);
---
+++ oldGame.js
@@ -0,0 +1,215 @@
+var SPEED = 180;
+var GRAVITY = 1200;
+var JUMP = 580;
+var ASSET_VERSION = (new Date()).getTime();
+var BASE_PATH = '';
+
+var state = {
+    preload: function () {
+        this.load.spritesheet("player", BASE_PATH + 'assets/tile-santa.png?' + ASSET_VERSION, 48, 48);
+        this.load.image("background.0", BASE_PATH + "assets/background.png?" + ASSET_VERSION, 1600, 200);
+        this.load.image("background.1", BASE_PATH + "assets/background-1.png?" + ASSET_VERSION, 1600, 200);
+        this.load.image("background.2", BASE_PATH + "assets/background-2.png?" + ASSET_VERSION, 1600, 200);
+        this.load.image("background.3", BASE_PATH + "assets/background-3.png?" + ASSET_VERSION, 1600, 200);
+        this.load.image("floor", BASE_PATH + "assets/floor.png?" + ASSET_VERSION, 800, 8);
+        this.load.image("chimney", BASE_PATH + "assets/chimney.png?" + ASSET_VERSION, 24, 96);
+        this.load.image("bullet", BASE_PATH + "assets/bullet.png?" + ASSET_VERSION, 5, 9);
+    },
+    create: function () {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.background0 = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'background.0');
+        this.background1 = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'background.1');
+        this.background2 = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'background.2');
+        this.background3 = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'background.3');
+
+        this.floor = this.add.sprite(0, this.world.height - 8, 'floor');
+        this.game.physics.enable(this.floor);
+        this.floor.body.immovable = true;
+
+        this.player = this.add.sprite(100, 0, 'player');
+        this.player.animations.add('run', [0, 1, 2, 3, 4, 5, 6, 7], 20, true);
+        this.player.animations.add('jump', [8], 1, false);
+        this.player.animations.add('broken', [9], 1, false);
+
+        this.createWeapon();
+
+
+        this.weapon.trackSprite(this.player, 50, 15, true);
+
+        this.player.animations.play('run');
+
+        this.game.physics.enable(this.player);
+        this.player.body.gravity.y = GRAVITY;
+
+        this.chimneys = this.add.group();
+            // this.text = this.add.text(100, 100, "asdasdsadasdsadsadds", {
+            //     fill: '#ffdd00',
+            //     align: 'center'
+            // });
+
+        // this.text.anchor.setTo(0.5, 0.5);
+        // this.text.fontSize = 30;
+
+        this.hints = this.add.group();
+
+        this.scoreText = this.add.text(
+            this.world.width / 2,
+            this.world.height / 3,
+            "",
+            {
+                fill: '#ffdd00',
+                align: 'center'
+            }
+        );
+        this.scoreText.anchor.setTo(0.5, 0.5);
+        this.scoreText.fontSize = 20;
+
+        this.reset();
+    },
+    update: function () {
+        this.game.physics.arcade.collide(this.player, this.floor);
+        this.game.physics.arcade.collide(this.chimneys, this.floor);
+
+        var obj = this;
+        this.chimneys.forEachAlive(function (chimney) {
+            if (chimney.body.right < 0) {
+                chimney.kill();
+                obj.spawnChimney();
+            }
+        });
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)
+            || this.game.input.activePointer.isDown
+        ) {
+            if (!this.gameStarted) {
+                this.start();
+            } else if (this.gameOver) {
+                this.reset();
+            } else {
+                if (this.player.body.touching.down)
+                    this.player.body.velocity.y = -650;
+            }
+        }
+
+        if (this.gameStarted && !this.gameOver)
+            this.addScore(Math.round(this.time.physicsElapsed * 100));
+
+        if (!this.gameOver) {
+            //fireButton
+            if (this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR).isDown) {
+                this.weapon.fire();
+            }
+
+            this.game.physics.arcade.overlap(this.player, this.chimneys, this.killByChimney, null, this);
+            this.game.physics.arcade.overlap(this.weapon.bullets, this.chimneys, this.attackChimney, null, this);
+
+            this.background0.tilePosition.x -= this.time.physicsElapsed * SPEED / 8;
+            this.background1.tilePosition.x -= this.time.physicsElapsed * SPEED / 5;
+            this.background2.tilePosition.x -= this.time.physicsElapsed * SPEED / 3;
+            this.background3.tilePosition.x -= this.time.physicsElapsed * SPEED;
+            if (!this.player.body.touching.down)
+                this.player.animations.play('jump');
+            else
+                this.player.animations.play('run');
+        } else this.player.animations.play('broken');
+    },
+
+    start: function () {
+        this.scoreText.setText("SCORE: " + this.score);
+        this.gameStarted = true;
+        this.spawnChimney();
+    },
+    reset: function () {
+        this.gameStarted = false;
+        this.gameOver = false;
+        this.score = 0;
+        this.scoreText.setText("TOUCH TO\nSTART GAME");
+
+        this.chimneys.forEachAlive(function (chimney) {
+            chimney.kill();
+        });
+    },
+    spawnChimney: function () {
+        var chimney = this.chimneys.create(
+            this.game.width - 24,
+            this.floor.body.top - 96,
+            'chimney'
+        );
+        chimney.hp = 5;
+        this.game.physics.arcade.enable(chimney);
+        chimney.body.velocity.x = -200;
+    },
+    killByChimney: function (player, chimney) {
+        chimney.body.velocity.x = 0;
+        this.showHint(player, 'Ouuuuchhh');
+        this.setGameOver();
+    },
+    attackChimney: function (bullet, chimney) {
+        chimney.hp -= 1;
+        bullet.kill();
+        this.showHint(this.player, 'Hit');
+        if (chimney.hp == 0) {
+            chimney.kill();
+            this.spawnChimney();
+        }
+    },
+    addScore: function (addWhat) {
+        this.score += addWhat;
+        this.scoreText.setText("SCORE: " + this.score);
+    },
+    showHint: function (focusOn, text) {
+        var hint = this.game.add.text(
+            focusOn.x,
+            focusOn.y,
+            text,
+            {
+                fill: '#ffdd00',
+                align: 'center'
+            }
+        );
+        hint.anchor.setTo(0.5, 0.5);
+        hint.fontSize = 20;
+
+        var move = this.game.add.tween(hint);
+        move.to({y: hint.y - 100, x: hint.x - 100 * Math.random() + 50}, 1000);
+        move.onComplete.add(function () {
+            hint.kill()
+        }, this);
+        move.start();
+
+    },
+    setGameOver: function () {
+        this.timeOver = this.game.time.now;
+        this.gameOver = true;
+
+        this.scoreText.setText("FINAL SCORE: " + this.score + "\nTOUCH TO TRY AGAIN");
+    },
+    createWeapon: function(){
+        //Weapon
+        //  Creates 30 bullets, using the 'bullet' graphic
+        this.weapon = game.add.weapon(500, 'bullet');
+
+        //  The bullet will be automatically killed when it leaves the world bounds
+        this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+
+        //  Because our bullet is drawn facing up, we need to offset its rotation:
+        this.weapon.bulletAngleOffset = 90;
+
+        //  The speed at which the bullet is fired
+        this.weapon.bulletSpeed = 100;
+
+        //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+        this.weapon.fireRate = 60;
+
+        return this.weapon;
+    }
+};
+
+var game = new Phaser.Game(
+    800,
+    200,
+    Phaser.CANVAS,
+    document.querySelector('#screen'),
+    state
+);
