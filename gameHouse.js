3d6f15f046d691e21186ed855ac450024d788267
DanielMathis <daniel.mathis@sector8.eu>
2017-12-02 12:32:06 +0100

    graphic upgrade

--- gameHouse.js
+++ gameHouse.js
@@ -20,9 +20,6 @@ var state = {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.world.setBounds(0, 0, 1920, 600);
 
-        floor = game.add.sprite(0, game.world.height - 8, 'floor');
-        game.physics.enable(floor);
-        floor.body.immovable = true;
 
         bg = game.add.tileSprite(0, 0, 1920, 600, 'background');
 
@@ -35,13 +32,19 @@ var state = {
         player.body.collideWorldBounds = true;
         player.body.gravity.y = 1000;
         player.body.maxVelocity.y = 1000;
-        // player.body.setSize(20, 32, 5, 16);
+        player.body.setSize(150, 133, 0, 0);
 
         player.animations.add('right',
             [4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31], 25, true);
         player.animations.add('turn', [4], 20, true);
         player.animations.add('left',
             [3, 2, 1, 0, 11, 10, 9, 8, 19, 18, 17, 16, 27, 26, 25, 24], 25, true);
+
+
+        floor = game.add.sprite(0, game.world.height - 50, 'floor');
+        game.physics.enable(floor);
+        floor.body.collideWorldBounds = true;
+        floor.body.immovable = true;
 
         cursors = game.input.keyboard.createCursorKeys();
         jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -54,8 +57,9 @@ var state = {
         cursors = game.input.keyboard.createCursorKeys();
     },
     update: function () {
-        // game.physics.arcade.collide(player, layer);
-        // game.physics.arcade.overlap(this.player, this.chimneys, this.killedByChimney, null, this);
+        game.physics.arcade.collide(player, floor);
+        game.physics.arcade.collide(floor, this.chimneys);
+        game.physics.arcade.overlap(player, this.chimneys, this.killedByChimney, null, this);
         game.physics.arcade.collide(player, this.chimneys);
 
         player.body.velocity.x = 0;
@@ -87,14 +91,14 @@ var state = {
                     player.frame = 0;
                 }
                 else {
-                    player.frame = 4;
+                    player.frame = 7;
                 }
 
                 facing = 'idle';
             }
         }
 
-        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
+        if (jumpButton.isDown && player.body.touching.down && game.time.now > jumpTimer) {
             player.body.velocity.y = -1000;
             jumpTimer = game.time.now + 750;
         }
@@ -107,7 +111,7 @@ var state = {
             'chimney'
         );
         this.game.physics.arcade.enable(chimney);
-        chimney.body.velocity.x = -200;
+        chimney.body.velocity.x = 0;
     }, render: function () {
         // game.debug.text(game.time.physicsElapsed, 32, 32);
         // game.debug.body(player);
@@ -122,7 +126,9 @@ var state = {
     },
     killedByChimney: function (player, chimney) {
         chimney.body.velocity.x = 0;
-        this.setGameOver();
+        chimney.body.velocity.y = 0;
+        this.showHint(player, "FUCK! I woke them up.")
+        // this.setGameOver();
     },
     addScore: function (addWhat) {
         this.score += addWhat;
@@ -134,7 +140,7 @@ var state = {
             focusOn.y,
             text,
             {
-                fill: '#ffdd00',
+                fill: '#ff1105',
                 align: 'center'
             }
         );
